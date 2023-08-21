56a1fdf58c56620d11d7a419ca38bb2f025a4b07
osman ijaz <osmanijaz@outlook.com>
2023-08-21 23:43:46 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -43,18 +43,21 @@ const App = () => {
   }
 
   if(tours.length === 0){
-    return <main>
-      <div className="title">
-        <h2>No Tours Left</h2>
-        <button type="button" style={{marginTop:'2rem'}} className="btn" onClick={()=>fetchTours}>Refresh</button>
-
-
-      </div>
-
-
-
-
-    </main>
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button
+            type="button"
+            style={{ marginTop: '2rem' }}
+            className="btn"
+            onClick={() => fetchTours()}
+          >
+            Refresh
+          </button>
+        </div>
+      </main>
+    )
   }
 
 
